da148a17adec56f1f5c73b034e920380b0ced7bc
Fluidinfo developers - ntoll <dev@fluidinfo.com>
2010-12-17 20:48:02 +0000

    Doh... fixed setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 
 setup(name='delicious2fluid',
-      version='0.0.2',
+      version='0.0.3',
       description="Imports delicious bookmarks to FluidDB",
       author='Nicholas Tollervey',
       author_email='ntoll@ntoll.org',
       url='http://fluidinfo.com',
       license='MIT',
       requires=['httplib2', ],
-      scripts=['delicious2fluid'],
+      scripts=['delicious2fluid.py', ],
       long_description=open('README.rst').read(),
       classifiers=['Development Status :: 4 - Beta',
                    'Environment :: Web Environment',
